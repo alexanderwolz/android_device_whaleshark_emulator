ddca2cf619dfcf16e0db291d8fcb28c7bd4db4a4
Alexander Wolz <alexander.wolz@gmx.de>
2022-02-28 10:59:13 +0100

    removed cluster display, but made it configurable

--- catfish.mk
+++ catfish.mk
@@ -5,13 +5,10 @@
 #	- device/generic/goldfish/64bitonly/product/sdk_phone64_arm64.mk
 #	- build/make/target/product/sdk_phone_arm64.mk (deprecated?)
 
-#TARGET_PREBUILT_KERNEL := kernel/prebuilts/5.10/arm64/kernel-5.10-gz:kernel-ranchu
-
 # Use own source properties
 PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
 
-
-# ## Android Automotive
+## Android Automotive
 
 PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
 
@@ -19,20 +16,29 @@ PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
 PRODUCT_SYSTEM_EXT_PROPERTIES += ro.setupwizard.mode?=OPTIONAL
 
 $(call inherit-product, device/generic/car/common/car.mk)
-# This overrides device/generic/car/common/car.mk
 $(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
 $(call inherit-product, device/generic/car/emulator/rotary/car_rotary.mk)
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
-    device/generic/car/emulator/cluster/display_settings.xml:system/etc/display_settings.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml 
 
+# Enable cluster display
+BUILD_EMULATOR_CLUSTER_DISPLAY := false
+ENABLE_CLUSTER_OS_DOUBLE := false
+
+# Cluster / co-driver display
+ifeq (true,$(BUILD_EMULATOR_CLUSTER_DISPLAY))
+PRODUCT_COPY_FILES += \
+    device/generic/car/emulator/cluster/display_settings.xml:system/etc/display_settings.xml
 PRODUCT_PRODUCT_PROPERTIES += \
     hwservicemanager.external.displays=1,400,600,120,0 \
     persist.service.bootanim.displays=8140900251843329
-
-#DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/osdouble_overlay
+ifeq (true,$(ENABLE_CLUSTER_OS_DOUBLE))
+DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/osdouble_overlay
+else
 DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/overlay
+endif  # ENABLE_CLUSTER_OS_DOUBLE
+endif  # BUILD_EMULATOR_CLUSTER_DISPLAY
 
 # Define the host tools and libs that are parts of the SDK.
 $(call inherit-product-if-exists, sdk/build/product_sdk.mk)

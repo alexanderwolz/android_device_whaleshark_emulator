aec637e32dbb816cb11e0f07f583914923652b88
Alexander Wolz <mail@alexanderwolz.de>
2023-12-17 14:39:25 +0100

    Updated device configurations to build against predefined sdk car configuration

--- whaleshark_emulator_arm64.mk
+++ whaleshark_emulator_arm64.mk
@@ -1,25 +1,12 @@
 # Copyright (C) 2023 Alexander Wolz, mail@alexanderwolz.de
-# based on:
-#	- device/generic/goldfish/car/sdk_car_arm64.mk
-#	- device/generic/car/emulator/aosp_car_emulator.mk
-#	- device/generic/goldfish/64bitonly/product/sdk_phone64_arm64.mk
-#	- build/make/target/product/sdk_phone_arm64.mk (deprecated?)
+# product based on:
+#	- device/generic/car/sdk_car_arm64.mk
 
-# TODO check out: with android-14, there now is:
-# device/generic/car/sdk_car_arm64.mk
+# Since Android 14: derive directly from device/generic/car/sdk_car_arm64.mk
+$(call inherit-product, device/generic/car/sdk_car_arm64.mk)
 
-## Android Automotive
-PRODUCT_PACKAGE_OVERLAYS := device/generic/car/common/overlay
-
-# Enable Setup Wizard. This overrides the setting in emulator_vendor.mk
-PRODUCT_SYSTEM_EXT_PROPERTIES += ro.setupwizard.mode?=OPTIONAL
-
-$(call inherit-product, device/generic/car/common/car.mk)
-$(call inherit-product, device/generic/car/emulator/audio/car_emulator_audio.mk)
-$(call inherit-product, device/generic/car/emulator/rotary/car_rotary.mk)
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml 
+# Use common definitions
+$(call inherit-product, device/alexanderwolz/whaleshark/common/whaleshark_common.mk)
 
 # Enable cluster display
 BUILD_EMULATOR_CLUSTER_DISPLAY := false
@@ -39,37 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += device/generic/car/emulator/cluster/overlay
 endif  # ENABLE_CLUSTER_OS_DOUBLE
 endif  # BUILD_EMULATOR_CLUSTER_DISPLAY
 
-# Define the host tools and libs that are parts of the SDK.
-$(call inherit-product-if-exists, sdk/build/product_sdk.mk)
-$(call inherit-product-if-exists, development/build/product_sdk.mk)
-
-
-## Android Common
-
-QEMU_USE_SYSTEM_EXT_PARTITIONS := true
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-#PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
-
-# All components inherited here go to system image
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_system.mk)
-
-# All components inherited here go to system_ext image
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
-
-# All components inherited here go to product image
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
-
-# All components inherited here go to vendor or vendor_boot image (from sdk_phone64_arm64)
-$(call inherit-product, device/generic/goldfish/arm64-vendor.mk)
-$(call inherit-product, device/generic/goldfish/64bitonly/product/emulator64_vendor.mk)
-$(call inherit-product, device/generic/goldfish/emulator64_arm64/device.mk)
-
 EMULATOR_VENDOR_NO_SOUND := true
-
-# Use common definitions
-$(call inherit-product, device/alexanderwolz/whaleshark/common/whaleshark_common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := alexanderwolz
@@ -77,5 +34,5 @@ PRODUCT_MANUFACTURER := alexanderwolz
 PRODUCT_NAME := whaleshark_emulator_arm64
 PRODUCT_MODEL := Whale Shark AAOS on ARM64 Emulator
 
-# we reuse the original device configuration from device/generic/goldfish/emulator64_arm64
-PRODUCT_DEVICE := emulator64_arm64
+# we reuse the original device configuration from device/generic/car/emulator_car64_arm64
+PRODUCT_DEVICE := emulator_car64_arm64
